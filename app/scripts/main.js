87b97b71989c017c25a705201bd1e941a4b76b81
Eugene <eugene.beloff@gmail.com>
2015-01-25 03:02:13 +0200

    add images and element on 1st slide

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,27 @@
 $(function(){
-	$('.reviews-list li').click(function(e) {
-		if(!$(this).hasClass('active')) {
-			$(this).addClass('active').siblings('li.active').removeClass('active');
-			$('.info').html($(this).find('.review-preview').html());
-		}
-	})
-
 	$(window).load(function(e) {
-		$('.content-holder').css('padding-bottom',$('.footer').height()+'px');
+		app.initialize();
+		app.stikFooter();
 	})	
 
 	$(window).resize(function(){
-		$('.content-holder').css('padding-bottom',$('.footer').height()+'px');
+		app.stikFooter();
 	})
+
+	window.app = {
+		initialize: function() {
+			$('.reviews-list li').click(function(e) {
+				if(!$(this).hasClass('active')) {
+					$(this).addClass('active').siblings('li.active').removeClass('active');
+					$('.info').html($(this).find('.review-preview').html());
+				}
+			})
+		},
+
+		stikFooter: function() {
+			$('.content-holder').css('padding-bottom',$('.footer').height()+'px');
+		}
+	}
+
+	
 });
